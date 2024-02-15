2bd5eb2f707675ed09b3f01f9fbeeff5ebc6efa9
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-02-15 21:20:32 +0100

    Implement update a counter

--- 07_practicing_tdd/counter.py
+++ 07_practicing_tdd/counter.py
@@ -16,3 +16,15 @@ def create_counter(name):
 
     COUNTERS[name] = 0
     return { name: COUNTERS[name] }, status.HTTP_201_CREATED
+
+
+@app.route("/counters/<name>", methods=["PUT"])
+def update_counter(name):
+    """Update a counter"""
+    app.logger.info(f"Request to update counter: {name}")
+
+    global COUNTERS
+    COUNTERS[name] += 1
+
+    app.logger.info(f"Counter: {name} is now {COUNTERS[name]}")
+    return { name: COUNTERS[name] }, status.HTTP_200_OK
